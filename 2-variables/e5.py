7794ca280a956b22f68ae64e4dba38eb9c940c58
Carlangasx <86724703+Carlangasx@users.noreply.github.com>
2025-01-11 11:48:03 -0400

    Update e5.py

--- 2-variables/e5.py
+++ 2-variables/e5.py
@@ -5,9 +5,9 @@ k: float = 8.85e-12
 while True:
     datos:str = input('Datos en la forma: q1 q2 r.SIN Espacios: ')
     if (len(datos) == 5):
-        q1:float = int(datos[0])
-        q2:float = int(datos[2])
-        r:float = int(datos[4])
+        q1:float = float(datos[0])
+        q2:float = float(datos[2])
+        r:float = float(datos[4])
         break
 
 magnitud:float = k * (q1 * q2) / (r**2) 
